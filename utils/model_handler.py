2da51211d7a17a1518bac48cde1847aaf1aca498
n2oneProgrammer <dominikwojtasik191@gmail.com>
2023-12-23 20:27:02 +0100

    get best model

--- utils/model_handler.py
+++ utils/model_handler.py
@@ -1,5 +1,20 @@
-def best_model() -> str:
-    # TODO: Find file best.pt in the newest version of model
-    # (versions/version[version_number]/weights/best.pt)
+import os.path
+import re
 
-    return "yolov8n.pt"
+default_model = "yolov8n.pt"
+
+
+def extract_number(f):
+    s = re.findall("\d+$", f)
+    return int(s[0]) if s else -1, f
+
+
+def best_model() -> str:
+    if not os.path.isdir(os.path.join(os.getcwd(), "versions")):
+        return default_model
+
+    versions = os.listdir("versions")
+    max_version = max(versions, key=extract_number)
+
+    return os.path.join(os.getcwd(), "versions", max_version, "weights", "best.pt")
+
