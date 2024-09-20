408a02412205c1551d1dc1693cce34eb89073c3d
n2oneProgrammer <dominikwojtasik191@gmail.com>
2024-09-20 17:32:56 +0200

    scripts v2

--- example_use.py
+++ example_use.py
@@ -1,26 +1,32 @@
+import math
 import os
-
 import cv2
 from ultralytics import YOLO
-
 from utils.model_handler import best_model
 
 x_line = 10
+conf_limit = 0.50
+input_dir = "./datasets/train/"
+out_dir = "./out/"
+if not os.path.isdir(out_dir):
+    os.mkdir(out_dir)
 
-files = [f for f in os.listdir("./datasets/valid/images")]
-model = YOLO(best_model())
+files = [f for f in os.listdir(os.path.join(input_dir, "images"))]
+model = YOLO(best_model("versions"))
 for file in files:
-    img = cv2.imread(os.path.join("./datasets/valid/images", file))
-    # load the pre-trained YOLOv8n model
-    results = model.predict(img)
+    img = cv2.imread(os.path.join(input_dir, "images", file))
+    results = model.predict(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
     for r in results:
         for box in r.boxes:
             b = box.xyxy[0]
-            if b[1] > x_line:
+            if b[1].item() > x_line and box.conf.item() > conf_limit:
                 c = box.cls
                 startpos = (int(b[0].item()), int(b[1].item()))
                 endpos = (int(b[2].item()), int(b[3].item()))
-                print(b)
-                print(startpos, endpos)
                 cv2.rectangle(img, startpos, endpos, (255, 0, 0), 2)
-    cv2.imwrite(os.path.join("./out/", file), img)
+                cv2.putText(img, r.names[c.item()] + " " + str(math.floor(box.conf.item() * 100)) + "%",
+                            (startpos[0], startpos[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
+                            (36, 255, 12),
+                            2)
+    print(os.path.join(out_dir, file))
+    cv2.imwrite(os.path.join(out_dir, file), img)
